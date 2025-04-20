17f9203348738d5582239e4e5edf1bd53fc67c93
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-20 16:49:13 +0300

    Add event handler to Keypad component to log 'Entering password

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
-// Code Keypad Component Here
+// Keypad.js
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from 'react';
+
+function Keypad() {
+  // Event handler for when the input changes
+  const handleChange = () => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      <input 
+        type="password" 
+        onChange={handleChange} 
+        placeholder="Enter password"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
